bce3f069158541f3a61a18f7a374c9a229bc539a
saitejakesa <kesasaiteja1996@gmail.com>
2023-09-27 09:11:48 +0530

    emojipublicapi_final

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-document.body.innerHTML=`
+const baseURL = " http://calapi.inadiutorium.cz/api/v0/en/calendars";
+
+const getAllData = async() => {
+  fetch(`${baseURL}`, { method: "GET" })
+  .then((data) => data.json())
+    .then((data) => {
+        console.log(data);
+        
+      
+        populateGridData(data);
+      })
+    .catch((er) => errorHandler(er));
+};
+
+const populateGridData = (data) => {
+    document.body.innerHTML=
+    
+    data.map((i)=>`
     <div class=fullscreen>
         <div class=image2> 
             <div class="allcards" id="cardsdata"></div>
         </div>   
     </div>
-`
-let emojis;
-const getData=async()=>{
-
-    try{
-        const data=await fetch("http://calapi.inadiutorium.cz/api/v0/en/calendars")
-        emojis=await data.json();
-        console.log(emojis)
-        //allcards.innerHTML=""
-        // emojis.map(key=>{
-        //     displayemojis(key)
-        // })
-
-    }
-    catch(error){
-        emojis.innerHTML = "Input is " + error
-    }
-    
-
+    `)
 }
-getData()
+const errorHandler = (er)=>console.log(er)
+getAllData()
 
 
 
